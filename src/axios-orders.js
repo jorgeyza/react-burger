6ff8edcfe0921e388565f9bce26413912f286bc5
Jorge Eyzaguirre <jorgeyza@gmail.com>
2020-08-04 17:09:18 -0500

    Change API keys and axios fetching routes && Use hooks in BurgerIngredient, OrderSummary and Modal

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://test-3e15a.firebaseio.com/'
+  baseURL: "https://react-my-burger-9e63c.firebaseio.com/",
 });
 
 export default instance;
