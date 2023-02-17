2365ce3e54204cb6e911e7527a703f9d6f0b6cb2
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-17 18:44:59 -0500

    Update to .mjs extension

---
+++ src/store/actions/index.mjs
@@ -0,0 +1,3 @@
+export { addIngredient, removeIngredient, initIngredients } from "./burgerBuilder";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order.mjs";
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
